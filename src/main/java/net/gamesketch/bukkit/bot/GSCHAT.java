89ef7babb232cbb4db79c6e61dc8c2836268e8ad
Streammz <robbygroot@quicknet.nl>
2011-04-04 20:56:19 +0200

    Added /blockhead
    Makes opped people able to change their head slot to anything.

--- src/main/java/net/gamesketch/bukkit/bot/GSCHAT.java
+++ src/main/java/net/gamesketch/bukkit/bot/GSCHAT.java
@@ -4,6 +4,7 @@ import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
@@ -75,6 +76,25 @@ public class GSCHAT extends JavaPlugin {
         	String distance = Integer.toString((int)playervect.distance(spawnvect));
             
         	player.sendMessage("The distance from you to the spawn is " + distance + " blocks.");
+        	return true;
+        }
+        if (commandName.equals("blockhead")) {
+        	Player player = (Player)sender;
+        	if (!player.isOp()) {
+        		sender.sendMessage("You don't have the right permissions to execute this command!");
+        		return false;
+        	}
+        	int itemid = 0;
+        	
+            try { 
+            	itemid = Integer.parseInt(args[0]);
+            }
+            catch (NumberFormatException e) { return false; }
+            if (itemid >= 255) { return false; }
+            
+        	ItemStack helm = new ItemStack(itemid);
+        	player.getInventory().setHelmet(helm);
+        	player.sendMessage("Enjoy your new block head!");
         	return true;
         }
         
