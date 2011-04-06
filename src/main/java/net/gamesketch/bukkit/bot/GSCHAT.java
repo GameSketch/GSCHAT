9a81eb289de662ff9099462ca69d743223c5383b
Streammz <robbygroot@quicknet.nl>
2011-04-06 16:47:14 +0200

    /freezetime + option to /blockhead <ID> <PLAYER>
    Also blockhead is in his own class

--- src/main/java/net/gamesketch/bukkit/bot/GSCHAT.java
+++ src/main/java/net/gamesketch/bukkit/bot/GSCHAT.java
@@ -1,11 +1,15 @@
 package net.gamesketch.bukkit.bot;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.event.Event;
+import org.bukkit.event.Event.Priority;
+import org.bukkit.event.player.PlayerListener;
 import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
 
@@ -16,24 +20,23 @@ import org.bukkit.util.Vector;
 * @author Streammz
 * 
 * 
-* TODO:
-* 2x longer day time
-* underwater breathing with helmet
-* compass to north
 * 
 */
 public class GSCHAT extends JavaPlugin {
-
+	private final PlayerListener playerListener = new playerListener();
+	public static boolean isTimeFrozen;
     public void onDisable() {
         //PluginManager pm = getServer().getPluginManager();
     }
 
     public void onEnable() {
-        //PluginManager pm = getServer().getPluginManager();
+        PluginManager pm = getServer().getPluginManager();
         PluginDescriptionFile pdfFile = this.getDescription();
-
+        pm.registerEvent(Event.Type.PLAYER_MOVE, playerListener, Priority.Normal, this);
+    	isTimeFrozen = false;
         // Print a startup message to the console, so we know it was started.
         System.out.println( pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!" );
+    
     }
 
     @Override
@@ -41,10 +44,6 @@ public class GSCHAT extends JavaPlugin {
         String commandName = command.getName().toLowerCase();
 
         if (commandName.equals("who")) {
-        	/*
-        	 * Translates the online players into a string.
-        	 * Author isn't me.
-        	 */
         	  Player[] online = sender.getServer().getOnlinePlayers();
           	  String list = "";
         	  int length = online.length - 1;
@@ -59,7 +58,7 @@ public class GSCHAT extends JavaPlugin {
         	sender.sendMessage("Connected players (" + serverliststring + "): " + list);
         	return true;
         }
-        if (commandName.equals("/spawn")) {
+        if (commandName.equals("/spawn") || commandName.equals("spawn")) {
         	Player player = (Player)sender;
         	player.teleportTo(player.getWorld().getSpawnLocation());
         	player.sendMessage("Welcome to the spawn!");
@@ -73,12 +72,14 @@ public class GSCHAT extends JavaPlugin {
         		return false;
         	}
         	player.getWorld().setSpawnLocation((int)playerloc.getX(), (int)playerloc.getY(), (int)playerloc.getZ());
-            return true;
+            player.sendMessage("You've set the spawn at your position.");
+        	return true;
         }
         if (commandName.equals("/dist")) {
         	Player player = (Player)sender;
         	Location spawnloc = player.getWorld().getSpawnLocation();
         	Location playerloc = player.getLocation();
+        	
         	
         	Vector spawnvect = new Vector();
         	spawnvect.setX(spawnloc.getX());
@@ -96,26 +97,29 @@ public class GSCHAT extends JavaPlugin {
         	return true;
         }
         if (commandName.equals("blockhead")) {
-        	Player player = (Player)sender;
-        	if (!player.isOp()) {
-        		sender.sendMessage("You don't have the right permissions to execute this command!");
-        		return false;
-        	}
-        	int itemid = 0;
-        	
-            try { 
-            	itemid = Integer.parseInt(args[0]);
-            }
-            catch (NumberFormatException e) { return false; }
-            if (itemid >= 255) { return false; }
-            
-        	ItemStack helm = new ItemStack(itemid);
-        	player.getInventory().setHelmet(helm);
-        	player.sendMessage("Enjoy your new block head!");
-        	return true;
+            boolean success = BlockHead.perform(sender, args);
+            if (success) { return true; }
+            else { return false; }
         }
         
+        if (commandName.equals("freezetime")) {
+        	Player player = (Player)sender;
+        	if (!player.isOp()) {
+        		player.sendMessage(ChatColor.RED + "You don't have the permissions to freeze time!");
+        		return false;
+        	}
+        	if (isTimeFrozen) {
+            	isTimeFrozen = !isTimeFrozen;
+            	player.sendMessage(ChatColor.AQUA + "The time isn't frozen anymore.");
+            	return true;
+        	} else {
+        		isTimeFrozen = !isTimeFrozen;
+        		player.sendMessage(ChatColor.AQUA + "The time is frozen until you reset now.");
+        		return true;
+        	}
+        }
         
+       
         return false;
     }
 
